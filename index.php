dae979647e032b3cb360661297138d9e01170160
Admin <Admin@MONET>
2015-11-06 20:58:15 +0530

    Inital Git Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Facebook Client-side OAuth Example</title>
+	<script src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js"></script>
+	<script src="./facebook.js"></script>
+    <link rel="stylesheet" href="styles.css" type="text/css">
+</head>
+<body>
+<h1 id="header">Get Facebook User Data</h1>
+<button id="fb-login">Login to Facebook</button>
+<pre id="userdata">
+
+</pre>
+</body>
+</html>
